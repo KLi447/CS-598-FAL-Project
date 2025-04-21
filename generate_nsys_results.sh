97fbaac7ab1f8ef4b0fd39d311cb40cc1c806b0b
Amaan Khan <amaanmkhan2@gmail.com>
2025-04-21 17:35:02 -0500

    Update script to remove sqlite file generation

--- generate_nsys_results.sh
+++ generate_nsys_results.sh
@@ -1,3 +1,4 @@
 # after running the profiling, run this to generate the .csv file from the nsys-rep output
-nsys export --type sqlite --output mlora_profile.sqlite mlora_profile.nsys-rep --force-overwrite true
+# don't need this first line anymore since run_profile.sh now generates the sqlite file
+# nsys export --type sqlite --output mlora_profile.sqlite mlora_profile.nsys-rep --force-overwrite true
 python scripts/performance_report.py --db mlora_profile.sqlite --output mlora_performance_report.csv
