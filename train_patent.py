9c1dcb5608aea32a28bec263093b27d562a689d3
Vir-Sikand <sikandvir@gmail.com>
2025-04-30 18:58:20 -0500

    added

--- train_patent.py
+++ train_patent.py
@@ -22,6 +22,7 @@ def main():
     parser.add_argument("--device", type=str, default="cuda:0", help="Device to use")
     parser.add_argument("--precision", type=str, default="fp32", help="Precision to use")
     parser.add_argument("--model_type", type=str, default="llama", help="Type of model (llama)")
+    parser.add_argument("--pipeline", action="store_true", help="Whether to use pipeline parallelism")
     args = parser.parse_args()
     
     # Set random seed for reproducibility
