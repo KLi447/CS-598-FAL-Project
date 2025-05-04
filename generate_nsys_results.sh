fc78acf1c3b7777dc11f46b0e60623d8bfa6b877
Amaan Khan <amaanmkhan2@gmail.com>
2025-05-04 15:42:50 -0500

    Add profile running scripts

---
+++ generate_nsys_results.sh
@@ -0,0 +1,4 @@
+
+# after running the profiling, run this to generate the .csv file from the nsys-rep output
+nsys export --type sqlite --output mlora_profile.sqlite mlora_profile.nsys-rep --force-overwrite true
+python scripts/performance_report.py --db mlora_profile.sqlite --output mlora_performance_report.csv
