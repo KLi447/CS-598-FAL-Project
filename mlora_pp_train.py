09c07c34cc6925efca1440d02372296f6c1afbdd
jeffbyju <59282955+jeffbyju@users.noreply.github.com>
2025-05-14 15:36:55 -0500

    readded license

--- mlora_pp_train.py
+++ mlora_pp_train.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 # m-LoRA: Efficient Multi-LoRA Fine Tuning with Shared-Based Model
-# [License header truncated for brevity]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (C) 2024 All Rights Reserved.
+#
+# Github:  https://github.com/TUDB-Labs/mLoRA
 
 import mlora.model
 import mlora.utils
