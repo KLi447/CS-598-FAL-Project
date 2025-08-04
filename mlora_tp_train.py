f7d59b35af697d40e7c52d29fedcc137b8d3fbef
KLi447 <kevinchenli44@gmail.com>
2025-08-03 23:53:14 -0500

    add missing file

---
+++ mlora_tp_train.py
@@ -0,0 +1,84 @@
+# m-LoRA: Efficient Multi-LoRA Fine Tuning with Shared-Based Model
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (C) 2024 All Rights Reserved.
+#
+# Github:  https://github.com/TUDB-Labs/mLoRA
+
+import mlora.model
+import mlora.utils
+import mlora.executor
+import mlora.config
+import torch
+import torch.distributed as dist
+import os
+import logging
+
+def setup_distributed(args):
+    try:
+        rank = int(os.environ["RANK"])
+        world_size = int(os.environ["WORLD_SIZE"])
+        local_rank = int(os.environ["LOCAL_RANK"])
+
+        dist.init_process_group(backend="nccl")
+
+        torch.cuda.set_device(local_rank)
+
+        args.rank = rank
+        args.nodes = world_size
+        args.device = f"cuda:{local_rank}"
+
+        logging.info(f"Distributed training enabled. Rank: {rank}/{world_size}, Device: {args.device}")
+        return True
+
+    except KeyError:
+        logging.info("Not a distributed run. Running in single-process mode.")
+        args.rank = 0
+        args.nodes = 1
+        return False
+
+if __name__ == "__main__":
+    args = mlora.utils.get_cmd_args()
+
+    is_distributed = setup_distributed(args)
+
+    try:
+
+        mlora.utils.setup_seed(args.seed)
+        mlora.utils.setup_logging(args.log_level, args.log_file)
+        mlora.utils.setup_cuda_check()
+        mlora.utils.setup_metric_logger(args.metric_file)
+
+        if args.trace:
+            mlora.utils.setup_trace_mode()
+
+        tokenizer, model = mlora.model.load_model(args)
+        config = mlora.config.MLoRAConfig(args.config)
+
+        executor = mlora.executor.TPExecutor(
+            model, tokenizer, config, args.device, args.rank, args.nodes
+        )
+
+        if args.rank == 0:
+            for item in config.tasks_:
+                executor.add_task(item)
+
+                executor.calculate_costs() #not implemented
+
+        executor.execute()
+
+    finally:
+        if is_distributed:
+            dist.destroy_process_group()
+            logging.info("Distributed process group destroyed.")
